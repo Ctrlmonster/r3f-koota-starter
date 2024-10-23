8d9ea8b203b205853a3bc410aa622ae27afa16b6
Brian Breiholz <thebrianbreiholz@gmail.com>
2024-10-23 15:07:16 +0200

    layout

--- src/js/ecs/systems.ts
+++ src/js/ecs/systems.ts
@@ -7,11 +7,55 @@ import {IsSphere, MeshRef, Position} from "./traits";
 
 // for demo purposes we store all systems in a single file
 
-// =================================================================================================================
+
+// =====================================================================================================================
+// =====================================================================================================================
 
 
-// demo-ing some movement. in a real game our physics, AI, character controller, w/e
+// this system will sync back the position values in our traits back to three.js meshes
+
+export const SyncPositionToThree = ({world}: { world: World }) => {
+  world.query(Position, MeshRef).updateEach(([pos, {ref: mesh}]) => {
+    // sync back to three
+    mesh.position.copy(pos);
+  });
+}
+
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+
+
+
+// this system will animate the colors based on position
+
+const orange = [255, 100, 0];
+const violet = [148, 0, 211];
+
+export const AnimateColors = ({world}: { world: World }) => {
+
+  world.query(Position, MeshRef).updateEach(([pos, {ref: mesh}]) => {
+
+    const distFromOrigin = mapLinear(Math.hypot(pos.x, pos.y, pos.z), 0, 25, 0, 1);
+    const r = orange[0] * distFromOrigin + (1 - distFromOrigin) * violet[0];
+    const g = orange[1] * distFromOrigin + (1 - distFromOrigin) * violet[1];
+    const b = orange[2] * distFromOrigin + (1 - distFromOrigin) * violet[2];
+
+    (mesh.material as MeshStandardMaterial).color.set(r / 255, g / 255, b / 255);
+  });
+
+}
+
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+
+
+// this system demos some movement. in a real game our physics, AI, character controller, w/e
 // would be setting the position
+
 
 let elapsedTime = 0;
 const Added = createAdded();
@@ -33,36 +77,4 @@ export const AnimateSpheres = ({world, delta}: { world: World, delta: number }) 
   });
 }
 
-// =================================================================================================================
 
-
-// this system will sync back the position values in our traits back to three.js meshes
-export const SyncPositionToThree = ({world}: { world: World }) => {
-  world.query(Position, MeshRef).updateEach(([pos, {ref: mesh}]) => {
-    // sync back to three
-    mesh.position.copy(pos);
-  });
-}
-
-
-// =================================================================================================================
-
-
-// this system will animate the colors based on position
-const orange = [255, 100, 0];
-const violet = [148, 0, 211];
-
-export const AnimateColors = ({world}: { world: World }) => {
-
-  world.query(Position, MeshRef).updateEach(([pos, {ref: mesh}]) => {
-
-    const distFromOrigin = mapLinear(Math.hypot(pos.x, pos.y, pos.z), 0, 25, 0, 1);
-    const r = orange[0] * distFromOrigin + (1 - distFromOrigin) * violet[0];
-    const g = orange[1] * distFromOrigin + (1 - distFromOrigin) * violet[1];
-    const b = orange[2] * distFromOrigin + (1 - distFromOrigin) * violet[2];
-
-    (mesh.material as MeshStandardMaterial).color.set(r / 255, g / 255, b / 255);
-  });
-
-}
-// =================================================================================================================
